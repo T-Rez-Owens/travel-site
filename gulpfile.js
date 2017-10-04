d2abe49f7a5f63a03bd400a8a729f1353c1e4550
Trezor <trezoro@mitylite.com>
2017-10-04 13:21:16 -0600

    updated a lot of the CSS files and added some automated tools like browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,2 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch');
-gulp.task('default', function(){
-	console.log("gulp task");
-});
-
-gulp.task('html', function(){
-	console.log("html task");
-});
-
-gulp.task('watch', function(){
-	watch('./app/index.html',function(){
-	gulp.start('html')
-	});
-});
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
