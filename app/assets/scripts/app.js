7370a063364ad186b7d7c4a45e8a190b45a2d3d9
Trezor <trezoro@mitylite.com>
2017-10-11 10:28:21 -0600

    Added Automation (compiling and error handling/checking/browser refreshing) for script files.

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,6 +1,11 @@
+var $ = require('jquery');
 var Person = require('./modules/Person');
+
+alert("this is a test 1234");
 
 var john = new Person("John Doe", "Green");
 john.greet();
 var jane = new Person("Jane Doe", "Green-blue");
 jane.greet();
+
+$('h1').remove();
